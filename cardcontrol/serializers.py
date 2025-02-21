8e369834f4027fdfbf136fc3d463d899f9a8d37c
ikakopa1985 <ikakopa1985@gmail.com>
2025-02-21 17:24:08 +0400

    3 commit

--- cardcontrol/serializers.py
+++ cardcontrol/serializers.py
@@ -5,11 +5,6 @@ from rest_framework import serializers
 
 
 class CardSerializer(serializers.ModelSerializer):
-    card_name = serializers.CharField(read_only=True)
-    card_account = serializers.CharField(read_only=True)
-    end_date = serializers.DateField(read_only=True)
-    is_active = serializers.BooleanField(read_only=True)
-
     class Meta:
         model = Card
         fields = ['user_id', 'card_name', 'card_account', 'end_date', 'is_active']
--- cardcontrol/views.py
+++ cardcontrol/views.py
@@ -1,21 +1,31 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
-from cardcontrol.serializers import *
-from cardcontrol.models import *
+from .models import Card
+from .serializers import CardSerializer
+from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
 
 
 
-# Create your views here.
-
-
 class CardViewset(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     serializer_class = CardSerializer
+    queryset = Card.objects.all()
 
-    def get_queryset(self):
-        user_id_received = self.request.query_params["user_id"]
+    def create(self, request, *args, **kwargs):
+        user_id = request.data.get("user")
+        if not user_id:
+            return Response({"error": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        print(user_id_received)
-        all_cards = Card.objects.filter(user__id=user_id_received)
-        return all_cards
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=user)  # ✅ მომხმარებელი ხელით მივაკუთვნოთ
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
